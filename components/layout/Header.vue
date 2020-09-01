589c576dc838756dfe5039b0d74a265f7566eaa7
ShaggyTech <beedat1@gmail.com>
2020-09-01 01:04:13 -0500

    chore: skeleton setup for new nuxt portfolio project

---
+++ components/layout/Header.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'Header',
+})
+</script>
+
+<template>
+  <v-app-bar app hide-on-scroll>
+    <v-app-bar-nav-icon />
+    <v-spacer />
+    <v-toolbar-title>
+      <v-btn to="/" nuxt exact tile block depressed x-large>
+        Brandon Eichler
+      </v-btn>
+    </v-toolbar-title>
+    <v-spacer />
+  </v-app-bar>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,93 @@
+import { NuxtConfig } from '@nuxt/types'
+
+const name = process.env.npm_package_name || ''
+const isDev = process.env.NODE_ENV !== 'production'
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES' ? { router: { base: `/${name}/` } } : {}
+
+const config: NuxtConfig = {
+  dev: isDev,
+  telemetry: false,
+  ...routerBase,
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: 'universal',
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'server',
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: ['@/plugins/composition-api.ts'],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: {
+    dirs: [
+      '~/components',
+      {
+        path: '~/components/layout/',
+        prefix: 'Layout',
+      },
+    ],
+  },
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/vuetify',
+    'nuxt-typed-vuex',
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa'],
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {
+    transpile: ['/typed-vuex/', 'vuetify/lib/util/colors'],
+  },
+  // Runtime type checking when running nuxt build
+  typescript: {
+    typeCheck: {
+      eslint: {
+        files: './**/*.{ts,js,vue}',
+      },
+    },
+  },
+  vuetify: {
+    optionsPath: '~/plugins/vuetify.ts',
+  },
+}
+
+export default config
